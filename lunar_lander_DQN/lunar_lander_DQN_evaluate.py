200885776052d30d64e7c9da60c6fd3f2c5d4873
oxcarxierra <oxcarxierra@gmail.com>
2024-07-11 16:02:22 +0900

    [feat] add lunar lander dqn model trained with GPU

--- lunar_lander_DQN/lunar_lander_DQN_evaluate.py
+++ lunar_lander_DQN/lunar_lander_DQN_evaluate.py
@@ -2,6 +2,7 @@ import gymnasium as gym
 import numpy as np
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 env = gym.make(
     "LunarLander-v2",
@@ -13,6 +14,19 @@ env = gym.make(
     turbulence_power=1.5,
 )
 
+class DQN(nn.Module):
+  def __init__(self, n_observations, n_actions):
+    super(DQN, self).__init__()
+    self.fc1 = nn.Linear(n_observations, 128)
+    self.fc2 = nn.Linear(128, 128)
+    self.fc3 = nn.Linear(128, n_actions)
+  
+  def forward(self, x):
+    x = F.relu(self.fc1(x))
+    x = F.relu(self.fc2(x))
+    x = self.fc3(x)
+    return x
+  
 run_device = torch.device(
     "cuda"
     if torch.cuda.is_available()
@@ -24,8 +38,9 @@ print(f"Running on {run_device}")
 TEST_NUM_EPISODES = 10
 MODEL_PATH = "models/lunar_lander_dqn_model.pth"
 
-model = torch.load(MODEL_PATH)
-model.eval()
+model = DQN(env.observation_space.shape[0], env.action_space.n)
+model = model.load_state_dict(torch.load(MODEL_PATH))
+# model.eval()
 
 for episode in range(TEST_NUM_EPISODES):
     state = env.reset()
